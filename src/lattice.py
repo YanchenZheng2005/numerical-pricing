aecc5abcf41a6219d325c28ff065b7cdb1648568
YanchenZheng2005 <zhengyanchen0829@gmail.com>
2026-09-16 00:54:00 -0600

    Add replicating portfolio tree for European lattice

--- src/lattice.py
+++ src/lattice.py
@@ -12,12 +12,35 @@ def Euro_option_A(S0, u, d, r, T, n, h):
     q = (R - d) / (u - d)
     option_price = [[0] * (n + 1) for _ in range(n + 1)]
     for i in range(n,-1,-1):
-        for j in range(i+1):
+        for j in range(i + 1):
             if i == n:
                 option_price[i][j] = h(stock_price[i][j])
             else:
                 option_price[i][j] = (q * option_price[i+1][j] + (1 - q) * option_price[i+1][j+1]) / R
     return option_price
+
+
+def portfolio_tree(S0, u, d, r, T, n, h):
+    stock_price = stock_lattice(S0, u, d, n)
+    option_price = Euro_option_A(S0, u, d, r, T, n, h)
+    stock_share_tree = [[0] * (n + 1) for _ in range(n + 1)]
+    bond_value_tree = [[0] * (n + 1) for _ in range(n + 1)]
+    R = math.exp(r * T / n)
+    for i in range(n):
+        for j in range(i + 1):
+            O_up = option_price[i + 1][j]
+            O_down = option_price[i + 1][j + 1]
+            S_up = stock_price[i + 1][j]
+            S_down = stock_price[i + 1][j + 1]
+
+            delta = (O_up - O_down) / (S_up - S_down)
+            B = (O_up - delta * S_up) / R
+
+            stock_share_tree[i][j] = delta
+            bond_value_tree[i][j] = B
+
+    return stock_share_tree, bond_value_tree
+
 
 def Euro_option_B(S0, u, d, r, T, n, h):
     stock_price = stock_lattice(S0, u, d, n)
